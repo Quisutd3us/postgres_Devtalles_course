7906a0e35e372128f76abb03c92738b1918a78b0
d3us <dnarino@gmail.com>
2023-10-26 18:21:51 -0500

    update fk to delete on cascade and test

--- queries/foreignKeys.sql
+++ queries/foreignKeys.sql
@@ -2,6 +2,20 @@ select *
 from country
 where code='AFG';
 
+delete
+from country
+where code='AFG';
+
+
+
+select *
+from city
+where countrycode='AFG';
+select *
+from countrylanguage
+where countrycode='AFG';
+
+
 INSERT INTO country
 		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');
 
