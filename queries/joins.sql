c0360759c7e2f9e9ab1aa72cf737a4e26618043c
d3us <dnarino@gmail.com>
2023-10-28 13:13:40 -0500

    full outer join

--- queries/joins.sql
+++ queries/joins.sql
@@ -5,10 +5,33 @@ select name,code from continent where code in (1,5)
 order by name asc;
 
 --inner join
-
-select * from continent;
-
-select a.name as country , b.name as continent from country a
+select a.name as country , b.name as continent
+from country a
 inner join continent b on b.code = a.continent
 order by  a.name asc;
 
+select A.name , B.name
+from country A
+inner join continent B
+    on A.continent= B.code
+where B.name in ('South America','North Asia');
+
+--restart sequence
+
+alter sequence continent_code_seq restart with 8;
+
+--insert rows in continent table
+
+insert into public.continent (name)
+values ('South Asia');
+
+
+select * from continent;
+
+--full outer join
+
+select A.name as CityName,A.continent as continentCode, B.name as continentName
+from country A
+full outer join continent B
+on B.code = A.continent
+order by a.name desc ;
