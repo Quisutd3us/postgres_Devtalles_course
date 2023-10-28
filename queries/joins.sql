55cc3130285e66771fb1c8a5ab45cbcc3ca5d30d
d3us <dnarino@gmail.com>
2023-10-28 15:14:32 -0500

    COUNT BY

--- queries/joins.sql
+++ queries/joins.sql
@@ -1,20 +1,24 @@
 --union
-select name,code from continent where name like '%America'
+select name, code
+from continent
+where name like '%America'
 union
-select name,code from continent where code in (1,5)
+select name, code
+from continent
+where code in (1, 5)
 order by name asc;
 
 --inner join
-select a.name as country , b.name as continent
+select a.name as country, b.name as continent
 from country a
-inner join continent b on b.code = a.continent
-order by  a.name asc;
+         inner join continent b on b.code = a.continent
+order by a.name asc;
 
-select A.name , B.name
+select A.name, B.name
 from country A
-inner join continent B
-    on A.continent= B.code
-where B.name in ('South America','North Asia');
+         inner join continent B
+                    on A.continent = B.code
+where B.name in ('South America', 'North Asia');
 
 --restart sequence
 
@@ -26,29 +30,49 @@ insert into public.continent (name)
 values ('South Asia');
 
 
-select * from continent;
+select *
+from continent;
 
 --full outer join
 
-select A.name as CityName,A.continent as continentCode, B.name as continentName
+select A.name as CityName, A.continent as continentCode, B.name as continentName
 from country A
-full outer join continent B
-on B.code = A.continent
-order by a.name desc ;
+         full outer join continent B
+                         on B.code = A.continent
+order by a.name desc;
 
 
 --RIGHT OUTER JOIN
 SELECT distinct B.NAME
 FROM country A
-RIGHT JOIN continent B
-ON A.continent = B.code
+         RIGHT JOIN continent B
+                    ON A.continent = B.code
 WHERE A.continent IS NULL
 ORDER BY B.name asc;
 
 --AGGREGATIONS
 
-select  count(*),B.name
+select count(*) as total, B.name
 from country A
-INNER JOIN continent B
-on A.continent = B.code
-GROUP BY A.continent, B.name;
+         INNER JOIN continent B
+                    on A.continent = B.code
+GROUP BY A.continent, B.name
+order by count(*) desc;
+
+
+--COUNT UNION
+
+(select count(*) as total, B.name
+from country A
+         inner join continent B
+                    ON A.continent = B.code
+where b.name not in ('Central America', 'South America', 'North America')
+group by B.name
+union
+select count(*)  as total,'America'
+from country A
+         inner join continent B
+                    ON A.continent = B.code
+where b.name in ('Central America', 'South America', 'North America'))
+order by total asc;
+
